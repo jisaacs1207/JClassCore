dd4a2b99f008a9d57418f206b9bfa2940a918990
jisaacs1207 <jisaacs1207@gmail.com>
2018-06-13 12:16:50 +0800

    Initial Commit

---
+++ src/main/java/io/jisaacs/ci/jclasscore/JClassCore.java
@@ -0,0 +1,17 @@
+package io.jisaacs.ci.jclasscore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JClassCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
